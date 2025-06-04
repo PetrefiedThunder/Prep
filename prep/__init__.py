ab27b24576d9e13d46dce09321a4b4f7cdbbca49
C.Lee <131207452+PetrefiedThunder@users.noreply.github.com>
2025-06-03 21:32:25 -0700

    Add basic package and tests

---
+++ prep/__init__.py
@@ -0,0 +1,5 @@
+"""Prep package placeholder"""
+
+def initialize():
+    """Placeholder initialize function."""
+    return True
---
+++ tests/prep/test_utility.py
@@ -0,0 +1,4 @@
+from prep import utility
+
+def test_util_func():
+    assert utility.util_func() is True
