ffd6132861ae30ff93924acae9297315ecf3c495
C.Lee <131207452+PetrefiedThunder@users.noreply.github.com>
2025-10-30 07:48:59 -0700

    feat: add inventory microservice and connectors

--- prep/models/__init__.py
+++ prep/models/__init__.py
@@ -1,6 +1,17 @@
 from .db import SessionLocal, engine, get_db_url
 from .guid import GUID
-from .orm import Base, Booking, BookingStatus, Kitchen, RecurringBookingTemplate
+from .orm import (
+    Base,
+    Booking,
+    BookingStatus,
+    InventoryItem,
+    InventoryLot,
+    InventoryTransfer,
+    InventoryTransferStatus,
+    Kitchen,
+    RecurringBookingTemplate,
+    Supplier,
+)
 
 __all__ = [
     "Base",
@@ -8,6 +19,11 @@ __all__ = [
     "Kitchen",
     "RecurringBookingTemplate",
     "BookingStatus",
+    "InventoryItem",
+    "InventoryLot",
+    "InventoryTransfer",
+    "InventoryTransferStatus",
+    "Supplier",
     "engine",
     "SessionLocal",
     "get_db_url",
