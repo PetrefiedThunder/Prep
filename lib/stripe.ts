2737705e89b26ceabe03a92c504a5f0ef34760b7
Christopher Sellers <131207452+PetrefiedThunder@users.noreply.github.com>
2025-12-11 12:57:52 -0800

    Fix Stripe apiVersion typing

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -10,5 +10,5 @@ const stripeSecretKey =
       })())
 
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2023-10-16' as any,
+  apiVersion: '2023-10-16' as unknown as Stripe.StripeConfig['apiVersion'],
 })
